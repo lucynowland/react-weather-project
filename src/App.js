af52c3519b03ab0a36da9966c9651e2cf69d6dc3
Lucy Nowland <lucynowland03@gmail.com>
2024-03-27 15:50:50 +0000

    Integrate OpenWeather API to retrieve weather data

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         {" "}
         <h1 className="header">React Weather App</h1>
-        <Weather />
+        <Weather defaultCity="Charleston" />
         <footer>
           This project was coded by{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { TailSpin } from "react-loader-spinner";
 import "./Weather.css";
-import Sunny from "./images/sunny-icon.png";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            {" "}
-            <input
-              type="search"
-              placeholder="Enter a City..."
-              className="form-control"
-              autoFocus="on"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ loaded: false });
+
+  function displayForecast(response) {
+    console.log(response.data);
+    setWeatherData({
+      loaded: true,
+      city: response.data.name,
+      date: "Wednesday 11:30",
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  if (weatherData.loaded) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              {" "}
+              <input
+                type="search"
+                placeholder="Enter a City..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="form-control btn btn-style w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="form-control btn btn-style w-100"
+        </form>
+        <h1>Charleston</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-1">
+          <div className="col-6">
+            <img
+              src={weatherData.icon}
+              alt={weatherData.description}
+              className=" float-left"
             />
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">°C</span>
           </div>
-        </div>
-      </form>
-      <h1>Charleston</h1>
-      <ul>
-        <li>Wednesday 11:30</li>
-        <li>Sunny</li>
-      </ul>
-      <div className="row mt-1">
-        <div className="col-6">
-          <img src={Sunny} alt="Sunny" className="float-left" />
-          <span className="temperature">25</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 5%</li>
-            <li>Humidity: 43%</li>
-            <li>Wind: 12km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)}km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(displayForecast);
+
+    return (
+      <div className="loader">
+        <TailSpin color="#eeedbe" height={120} />
+      </div>
+    );
+  }
 }
