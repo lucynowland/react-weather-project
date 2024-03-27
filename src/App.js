612de203122faa9ca77f02acf2ec5a6e430647d4
Lucy Nowland <lucynowland03@gmail.com>
2024-03-27 11:13:27 +0000

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,33 +3,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <p>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/lucynowland"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Lucy Nowland
-        </a>{" "}
-        which is open-sourced on{" "}
-        <a
-          href="https://github.com/lucynowland/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://weather-application-react-project.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </p>
+      <div className="container">
+        {" "}
+        <h1>React Weather App</h1>
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/lucynowland"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lucy Nowland
+          </a>{" "}
+          which is open-sourced on{" "}
+          <a
+            href="https://github.com/lucynowland/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://weather-application-react-project.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
