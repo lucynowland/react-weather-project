d7468440ff232a20d4697f6f7d09527836ee81a8
Lucy Nowland <lucynowland03@gmail.com>
2024-03-27 10:35:05 +0000

    Add the GitHub repo & Netlify URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 
 export default function App() {
-  <div className="App">
-    <h1>React Weather App</h1>
-  </div>;
+  return (
+    <div className="App">
+      <h1>React Weather App</h1>
+      <p>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/lucynowland"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Lucy Nowland
+        </a>{" "}
+        which is open-sourced on{" "}
+        <a
+          href="https://github.com/lucynowland/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://weather-application-react-project.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </p>
+    </div>
+  );
 }
