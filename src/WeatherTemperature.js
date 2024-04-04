81ac5bab12e3e57e36a90f726312e903d39d5ec1
Lucy Nowland <lucynowland03@gmail.com>
2024-04-04 13:38:53 +0100

    Add useEffect to update forecast data depending on the change in coordinates

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,48 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("Fahrenheit");
-  }
-
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          <a href="/" onClick={convertToCelsius}>
-            °C
-          </a>{" "}
-          |{" "}
-          <a href="/" onClick={convertToFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = Math.round((props.celsius * 9) / 5 + 32);
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{fahrenheit}</span>
-        <span className="unit">
-          <a href="/" onClick={convertToCelsius}>
-            °C
-          </a>{" "}
-          |{" "}
-          <a href="/" onClick={convertToFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">
+        <a href="/">°C</a>
+      </span>
+    </div>
+  );
 }
